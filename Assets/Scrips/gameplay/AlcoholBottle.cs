b49b14f13c044b357cf96a562b34fc5d162641f5
Keres1232 <162938616+Keres1232@users.noreply.github.com>
2026-05-24 14:25:38 -0500

    cambio botella

--- Assets/Scrips/gameplay/AlcoholBottle.cs
+++ Assets/Scrips/gameplay/AlcoholBottle.cs
@@ -2,7 +2,51 @@ using UnityEngine;
 
 public class AlcoholBottle : MonoBehaviour
 {
+    [Header("Drunk")]
     [SerializeField] private float customDrunkAmount = 0f;
+
+    [Header("Floating Animation")]
+    [SerializeField] private float rotationSpeed = 80f;
+
+    [SerializeField] private float floatHeight = 0.25f;
+
+    [SerializeField] private float floatSpeed = 2f;
+
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        RotateBottle();
+
+        FloatBottle();
+    }
+
+    private void RotateBottle()
+    {
+        transform.Rotate(
+            Vector3.up *
+            rotationSpeed *
+            Time.deltaTime,
+            Space.World
+        );
+    }
+
+    private void FloatBottle()
+    {
+        Vector3 pos = _startPosition;
+
+        pos.y +=
+            Mathf.Sin(
+                Time.time * floatSpeed
+            ) * floatHeight;
+
+        transform.position = pos;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
